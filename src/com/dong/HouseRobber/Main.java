c1a949a7bc696873d7c402f84f61417d82f2b0c7
Dong <690443961@qq.com>
2020-09-14 11:06:59 +0800

    计算偷窃金额最高的偷窃方案（即偷哪些房子）

--- src/com/dong/HouseRobber/Main.java
+++ src/com/dong/HouseRobber/Main.java
@@ -1,5 +1,9 @@
 package com.dong.HouseRobber;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 0, 4 };
@@ -25,5 +29,41 @@ public class Main {
             dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
         }
         return dp[n];
+    }
+
+    /**
+     * 使用 back 数组 DP 数组仍然是其原始的形式，记录 f(k) 的值，即「偷前 k 间房子的最大金额」； Back 数组中的值要么是 -1，要么是
+     * -2。如果 back[k] = -1，表示 dp[k] 由 dp[k-1] 计算而来； 如果 back[k] = -2，表示 dp[k] 由
+     * dp[k-2] 计算而来。
+     */
+    public static List<Integer> robForBack(int[] nums) {
+        int N = nums.length;
+        int[] dp = new int[N + 1];
+        int[] back = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        back[1] = -2;
+        for (int k = 2; k <= N; k++) {
+            // 计算 DP 数组与 back 数组
+            if (dp[k - 1] >= dp[k - 2] + nums[k - 1]) {
+                dp[k] = dp[k - 1];
+                back[k] = -1;
+            } else {
+                dp[k] = dp[k - 2] + nums[k - 1];
+                back[k] = -2;
+            }
+        }
+
+        // 根据 back 数组得到具体偷窃方案
+        List<Integer> res = new ArrayList<>();
+        int k = N; // 从 dp[N] 开始向前寻找
+        while (k > 0) {
+            if (back[k] == -2) {
+                res.add(k - 1); // 确定要偷的房子
+            }
+            k = k + back[k]; // 根据 back 数组移动指针 k
+        }
+        Collections.reverse(res); // 由于是倒序向前寻找，最后还要反转一下
+        return res;
     }
 }
